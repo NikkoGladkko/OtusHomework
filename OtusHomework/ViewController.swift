8c49df1c4f6bd2d3dfe86f031d2365fec5ed3714
Николай Гладковский <ngladkovskiy@vtb.ru>
2022-11-28 18:39:34 +0300

    makeNewModule

---
+++ OtusHomework/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  OtusHomework
+//
+//  Created by Гладковский Николай Сергеевич on 28.11.2022.
+//
+
+import Foundation
+import UIKit
+
+class OtusHomeworkViewController: UIViewController {
+    
+}
